1a9686750ff82105639296761bb31d3dcafab668
poteto <naonyan1135@gmail.com>
2024-07-29 17:32:20 +0900

    今はメモリストア使ってるからexprssサーバー再起動したらなくなる

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const express = require("express")
 const app = express()
 const session = require("express-session")
 
-app.use(
-  session({
-    secret: "secret",
-  })
-)
+const sessionOptions = {
+  secret: "secret",
+  resave: false, //* 必ず保存する(変更があるかどうかにかかわらず)
+  saveUninitialized: false,
+}
+
+app.use(session(sessionOptions))
 
 app.get("/viewcount", (req, res) => {
-  if(req.session.count){
+  if (req.session.count) {
     console.log(req.session.count)
     req.session.count += 1
-  }  else {
-  req.session.count = 1 //* 0じゃだめ
-  }//* 自分でプロパティ設定
+  } else {
+    req.session.count = 1 //* 0じゃだめ
+  } //* 自分でプロパティ設定
   // console.log(req.session.count)
   res.send(`${req.session.count}回`)
 })
