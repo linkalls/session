eb46672e7b8ce004de97c936e4f27b293d122cfe
poteto <naonyan1135@gmail.com>
2024-07-29 17:36:50 +0900

    a

--- index.js
+++ index.js
@@ -12,13 +12,24 @@ app.use(session(sessionOptions))
 
 app.get("/viewcount", (req, res) => {
   if (req.session.count) {
-    console.log(req.session.count)
     req.session.count += 1
   } else {
     req.session.count = 1 //* 0じゃだめ
   } //* 自分でプロパティ設定
   // console.log(req.session.count)
   res.send(`${req.session.count}回`)
+})
+
+app.get("/register", (req, res) => {
+  const { username = "名無し" } = req.query
+  req.session.username = username
+  res.redirect("/greet")
+})
+
+app.get("/greet", (req, res) => {
+  const { username} = req.session
+  req.session.username = username
+  res.send(username)
 })
 
 app.listen(3000, () => {
